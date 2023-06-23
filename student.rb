477452af5f92a0665a230ff3f8abd887c6f257c0
ehmaddd <ehmaddd@gmail.com>
2023-06-23 14:01:32 +0500

    Add basic UI Files

--- student.rb
+++ student.rb
@@ -5,8 +5,8 @@ module StudentClass
   class Student < Person
     attr_reader :name
 
-    def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-      super(age, name, parent_permission)
+    def initialize(age, classroom, name = 'Unknown', parent_permission = true)
+      super(age, name)
       @classroom = classroom
     end
 
